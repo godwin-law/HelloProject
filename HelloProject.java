643e4222980346c61a1fd239fd95100a436f1a94
godwin-law <62929797+godwin-law@users.noreply.github.com>
2020-04-07 21:43:43 +0530

    Initial Commit

---
+++ HelloProject.java
@@ -0,0 +1,13 @@
+public class HelloProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World1");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		System.out.println("Hello World4");
+		System.out.println("Hello World5");
+	}
+
+}
